6cc4098352a6123b1658d6f8888cdd3161aa453f
KHAZRI Omar <omar.khazri@soprahr.com>
2024-06-21 16:16:22 +0100

    Add app setting.

---
+++ src/test/java/com/sesame/e_pharmacy/EPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesame.e_pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
